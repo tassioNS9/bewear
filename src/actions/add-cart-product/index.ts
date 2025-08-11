2a521bfd38a66dcb515e952210303dfe66f1305b
Tassio Neves <tassions@hotmail.com>
2025-08-11 09:04:42 -0300

    feat: add product to cart action

---
+++ src/actions/add-cart-product/index.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+
+import { db } from "@/db";
+import { cartItemTable, cartTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import { AddProductToCartSchema, addProductToCartSchema } from "./schema";
+
+export const addProductToCart = async (data: AddProductToCartSchema) => {
+// Verifica se os dados são válidos!
+  addProductToCartSchema.parse(data);
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  // verifica se o usuário está autenticado primeiro
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
+  const productVariant = await db.query.productVariantTable.findFirst({
+    where: (productVariant, { eq }) =>
+      eq(productVariant.id, data.productVariantId),
+  });
+  if (!productVariant) {
+    throw new Error("Product variant not found");
+  }
+  const cart = await db.query.cartTable.findFirst({
+    where: (cart, { eq }) => eq(cart.userId, session.user.id),
+  });
+  let cartId = cart?.id;
+  if (!cartId) {
+    // [newCart] já retorna o primeiro elemento da lista
+    const [newCart] = await db
+      .insert(cartTable)
+      .values({
+        userId: session.user.id,
+      })
+      .returning();
+    cartId = newCart.id;
+  }
+  const cartItem = await db.query.cartItemTable.findFirst({
+    where: (cartItem, { eq }) =>
+      eq(cartItem.cartId, cartId) &&
+      eq(cartItem.productVariantId, data.productVariantId),
+  });
+  if (cartItem) {
+    await db
+      .update(cartItemTable)
+      .set({
+        quantity: cartItem.quantity + data.quantity,
+      })
+      .where(eq(cartItemTable.id, cartItem.id));
+    return;
+  }
+  await db.insert(cartItemTable).values({
+    cartId,
+    productVariantId: data.productVariantId,
+    quantity: data.quantity,
+  });
+};
