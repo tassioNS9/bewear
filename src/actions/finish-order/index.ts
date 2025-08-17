93429cb72f96aa831bd50ee4e5ff5afa87d9568e
Tassio Neves <tassions@hotmail.com>
2025-08-17 19:05:27 -0300

    feat: ensure order can be created

---
+++ src/actions/finish-order/index.ts
@@ -0,0 +1,92 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+
+import { db } from "@/db";
+import {
+  cartItemTable,
+  cartTable,
+  orderItemTable,
+  orderTable,
+} from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+export const finishOrder = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  const cart = await db.query.cartTable.findFirst({
+    where: eq(cartTable.userId, session.user.id),
+    with: {
+      shippingAddress: true,
+      items: {
+        with: {
+          productVariant: true,
+        },
+      },
+    },
+  });
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+  if (!cart.shippingAddress) {
+    // verifica pois se o endereço de entrega está definido
+    // se não estiver, o usuário deve ser redirecionado para a página de identificação
+    throw new Error("Shipping address not found");
+  }
+  const totalPriceInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0,
+  );
+  let orderId: string | undefined;
+  await db.transaction(async (tx) => {
+    // a transação garante que todas as operações de banco de dados
+    // sejam concluídas com sucesso ou nenhuma delas seja aplicada
+    if (!cart.shippingAddress) {
+      throw new Error("Shipping address not found");
+    }
+    const [order] = await tx
+      .insert(orderTable)
+      .values({
+        email: cart.shippingAddress.email,
+        zipCode: cart.shippingAddress.zipCode,
+        country: cart.shippingAddress.country,
+        phone: cart.shippingAddress.phone,
+        cpfOrCnpj: cart.shippingAddress.cpfOrCnpj,
+        city: cart.shippingAddress.city,
+        complement: cart.shippingAddress.complement,
+        neighborhood: cart.shippingAddress.neighborhood,
+        number: cart.shippingAddress.number,
+        recipientName: cart.shippingAddress.recipientName,
+        state: cart.shippingAddress.state,
+        street: cart.shippingAddress.street,
+        userId: session.user.id,
+        totalPriceInCents,
+        shippingAddressId: cart.shippingAddress!.id,
+      })
+      .returning();
+    if (!order) {
+      throw new Error("Failed to create order");
+    }
+    orderId = order.id;
+    const orderItemsPayload: Array<typeof orderItemTable.$inferInsert> =
+      cart.items.map((item) => ({
+        orderId: order.id,
+        productVariantId: item.productVariant.id,
+        quantity: item.quantity,
+        priceInCents: item.productVariant.priceInCents,
+      }));
+    await tx.insert(orderItemTable).values(orderItemsPayload);
+    await tx.delete(cartTable).where(eq(cartTable.id, cart.id));
+    await tx.delete(cartItemTable).where(eq(cartItemTable.cartId, cart.id));
+  });
+  if (!orderId) {
+    throw new Error("Failed to create order");
+  }
+  return { orderId };
+};
--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,18 +1,68 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
 
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 const FinishOrderButton = () => {
-  const handleFinishOrder = async () => {};
+  const finisihOrderMutation = useFinishOrder();
+  const [sucessDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
   return (
     <>
       <Button
         className="w-full rounded-full"
         size="lg"
-        onClick={handleFinishOrder}
+        onClick={() => finisihOrderMutation.mutate()}
+        disabled={finisihOrderMutation.isPending}
       >
-        Finalizar compra
+        {finisihOrderMutation.isPending ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          "Finalizar pedido"
+        )}
       </Button>
+
+      <Dialog open={sucessDialogIsOpen} onOpenChange={setSuccessDialogIsOpen}>
+        <DialogContent>
+          <Image
+            src="/ilustration.svg"
+            alt="Pedido realizado com sucesso"
+            width={300}
+            height={300}
+            className="mx-auto"
+          />
+          <DialogTitle className="mt-4 text-2xl">
+            Pedido realizado com sucesso
+          </DialogTitle>
+          <DialogDescription className="font-medium">
+            Seu pedido foi realizado com sucesso! Você pode acompanhar o status
+            na seção de Meus Pedidos.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Button
+              className="rounded-full"
+              size="lg"
+              onClick={() => setSuccessDialogIsOpen(false)}
+            >
+              Ver meus pedidos
+            </Button>
+            <Button className="rounded-full" size="lg" variant="outline">
+              Voltar para a loja
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
