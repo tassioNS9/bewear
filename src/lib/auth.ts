e45289dae51457c3377e8cdce9bdaf2df3a20875
Tassio Neves <tassions@hotmail.com>
2025-08-21 19:36:41 -0300

    refactor: fix imports ui components

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,12 +8,6 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
